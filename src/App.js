5d643879b5ed98ecfdbd3805f43be54385c38009
Rob Caldecott <robert.caldecott@keyloop.com>
2021-07-15 12:25:45 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Box from "@material-ui/core/Box";
+import yellow from "@material-ui/core/colors/yellow";
+import grey from "@material-ui/core/colors/grey";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ backgroundColor: yellow[500], color: grey[900], padding: 4 }}>
+      Yellow box
+    </Box>
   );
 }
 
